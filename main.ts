489d6e0bbf1f516fdfcc29e30d9f50ba1a29ec6a
Piyush Chauhan <piyushchauhan2011@gmail.com>
2018-02-07 22:38:38 +0800

    Initial Commit

---
+++ main.ts
@@ -0,0 +1,60 @@
+import { props, withComponent } from "skatejs";
+import withLitHtml from "@skatejs/renderer-lit-html";
+import { html } from "lit-html";
+
+class WithLitHtml extends withComponent(withLitHtml()) {
+  state = {
+    value: "",
+    position: {
+      x: 0,
+      y: 0
+    }
+  };
+
+  static get props() {
+    return {
+      name: props.string
+    };
+  }
+
+  connecting() {
+    this.addEventListener('mousemove', (e: MouseEvent) => {
+      this.state = {
+        ...this.state,
+        position: {
+          x: e.clientX,
+          y: e.clientY
+        }
+      }
+    })
+  }
+
+  shouldUpdate(prevProps, prevState) {
+    return prevState !== this.state
+  }
+
+  change() {
+    this.state = { ...this.state, value: "Hey There!" };
+  }
+
+  render({ props, state }) {
+    return html`
+      <section>
+        <h1>Hello, ${props.name}!</h1>
+        <h2>X: ${state.position.x}, Y:${state.position.y}</h2>
+        <p>${state.value}</p>
+      </section>
+      <style>
+      section {
+        width: 100vw;
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        justify-content: center;
+      }
+      </style>
+    `;
+  }
+}
+
+customElements.define("with-lit-html", WithLitHtml);
